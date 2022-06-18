81017ed64be304b4c80aca3684e5982c4af9ea95
Sandy Instructor <sandyludosky@gmail.com>
2022-06-18 12:36:25 +0200

    topics

---
+++ course_files/02/my-next-app/pages/api/topics.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        id: 'angular', 
+        about: 'Angular is a development platform, built on TypeScript including a component-based framework for building scalable web applications, A collection of well-integrated libraries that cover a wide variety of features, including routing, forms management, client-server communication, and more and a suite of developer tools to help you develop, build, test, and update your code'
+    }, 
+    {
+        id: 'vue', 
+        about: 'Vue is a progressive framework for building user interfaces. It is designed from the ground up to be incrementally adoptable, and can easily scale between a library and a framework depending on different use cases. It consists of an approachable core library that focuses on the view layer only, and an ecosystem of supporting libraries that helps you tackle complexity in large Single-Page Applications.'
+    }
+]
