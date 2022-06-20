047252a672d28ec576e93a2738881eb2f469c803
Sandy Instructor <sandyludosky@gmail.com>
2022-06-20 02:27:36 +0200

    03_02_b

---
+++ course_files/03/03-02_getStaticProps/.next/server/pages/news.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/news";
+exports.ids = ["pages/news"];
+exports.modules = {
+
+/***/ "./pages/news/index.js":
+/*!*****************************!*\
+  !*** ./pages/news/index.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ News),\n/* harmony export */   \"getStaticProps\": () => (/* binding */ getStaticProps)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nfunction News() {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n        children: \"Top Stories\"\n    }, void 0, false, {\n        fileName: \"/Users/sandyinstructor/Desktop/learning-nextjs-2491193/course_files/03/03-02_getStaticProps/pages/news/index.js\",\n        lineNumber: 2,\n        columnNumber: 12\n    }, this);\n};\nconst API_KEY = \"9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ\";\nasync function getStaticProps() {\n    // The value of the `props` key will be\n    //  passed to the `Home` component\n    return {\n        props: {}\n    };\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9uZXdzL2luZGV4LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTtBQUFlLFNBQVNBLElBQUksR0FBRztJQUMzQixxQkFBTyw4REFBQ0MsSUFBRTtrQkFBQyxhQUFXOzs7OztZQUFLLENBQUM7Q0FDL0I7QUFFRCxNQUFNQyxPQUFPLEdBQUcsa0NBQWtDO0FBQzNDLGVBQWVDLGNBQWMsR0FBRztJQUNyQyx1Q0FBdUM7SUFDdkMsa0NBQWtDO0lBQ2xDLE9BQU87UUFDTEMsS0FBSyxFQUFFLEVBQUU7S0FDVjtDQUNGIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vMi42Ly4vcGFnZXMvbmV3cy9pbmRleC5qcz83YWEyIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIE5ld3MoKSB7XG4gICAgcmV0dXJuKDxoMT5Ub3AgU3RvcmllczwvaDE+KVxufVxuXG5jb25zdCBBUElfS0VZID0gXCI5aFV2T3FHR2RuQ0J2R0tnNEVCM0w3bUdkQkM4aEtLSlwiXG5leHBvcnQgYXN5bmMgZnVuY3Rpb24gZ2V0U3RhdGljUHJvcHMoKSB7XG4gIC8vIFRoZSB2YWx1ZSBvZiB0aGUgYHByb3BzYCBrZXkgd2lsbCBiZVxuICAvLyAgcGFzc2VkIHRvIHRoZSBgSG9tZWAgY29tcG9uZW50XG4gIHJldHVybiB7XG4gICAgcHJvcHM6IHt9XG4gIH1cbn0iXSwibmFtZXMiOlsiTmV3cyIsImgxIiwiQVBJX0tFWSIsImdldFN0YXRpY1Byb3BzIiwicHJvcHMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/news/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/news/index.js"));
+module.exports = __webpack_exports__;
+
+})();
