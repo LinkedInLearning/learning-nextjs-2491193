3309f6787d71a0d0db4e067843158dfb537690de
Sandy Instructor <sandyludosky@gmail.com>
2022-06-20 12:09:22 +0200

    03_03_e

--- course_files/03/03-02_getStaticProps/pages/news/index.js
+++ course_files/03/03-02_getStaticProps/pages/news/index.js
@@ -1,16 +1,24 @@
 import Layout from "../../components/Layout"
-export default function News() {
+
+export default function News({ results }) {
     return(
-    <Layout>
-      <h1>Top Stories</h1>
-    </Layout>)
+      <Layout>
+        <h1>Top Stories</h1>
+      </Layout>
+    )
 }
 
 const API_KEY = "9hUvOqGGdnCBvGKg4EB3L7mGdBC8hKKJ"
 export async function getStaticProps() {
+  const URL = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${API_KEY}`
+  const response = await fetch(URL)
+  const data = await response.json()
+  console.log(data.results)
   // The value of the `props` key will be
   //  passed to the `Home` component
   return {
-    props: {}
+    props: {
+      results : data.results
+    }
   }
 }
