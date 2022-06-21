d82cc3d4429694fed4161dc11f3b610365059141
Sandy Instructor <sandyludosky@gmail.com>
2022-06-21 13:40:45 +0200

    04_04_b

---
+++ course_files/04/04-04_image/pages/404.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout"
+export default function NotFound() {
+    return(
+        <Layout>
+            <h1>Not Found</h1>
+            <style jsx>
+                {`
+                    h1 {
+                        color: chocolate;
+                    }
+                `}
+            </style>
+        </Layout>
+    )
+}
